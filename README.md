c7105b72fa1121353d1c5f004059702fa05808a8
Jeff Standen <jeff@webgroupmedia.com>
2017-01-02 17:41:10 -0800

    * [Plugins/Slack] Implemented the Slack service provider for connected accounts.

---
+++ README.md
@@ -0,0 +1,29 @@
+Cerb Plugins - wgm.google
+===========================================
+Copyright (C) 2017 Webgroup Media, LLC.
+[http://cerb.io/](http://cerb.io/)
+
+What's this?
+------------
+This plugin provides integration with [Slack](https://www.slack.com/) via their API. This plugin is intended to be a bridge used by other plugins to communicate with Slack services.
+
+Installation
+------------
+* Make a new [Slack application](https://api.slack.com/apps).
+* Download the plugin **Setup->Plugins->Plugin** Library.
+* Enable the plugin and configure your Slack app credentials.
+
+Credits
+-------
+This plugin was developed by [WebGroup Media, LLC](http://www.cerberusweb.com/).
+
+License
+-------
+
+[http://opensource.org/licenses/gpl-2.0.php](http://opensource.org/licenses/gpl-2.0.php)  
+
+This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
---
+++ api/App.php
@@ -0,0 +1,205 @@
+<?php
+if(class_exists('Extension_PageMenuItem')):
+class WgmSlack_SetupMenuItem extends Extension_PageMenuItem {
+	const POINT = 'wgm.slack.setup.menu';
+	
+	function render() {
+		$tpl = DevblocksPlatform::getTemplateService();
+		$tpl->assign('extension', $this);
+		$tpl->display('devblocks:wgm.slack::setup/menu_item.tpl');
+	}
+};
+endif;
+
+if(class_exists('Extension_PageSection')):
+class WgmSlack_SetupSection extends Extension_PageSection {
+	const ID = 'wgm.slack.setup.page';
+	
+	function render() {
+		$tpl = DevblocksPlatform::getTemplateService();
+		$visit = CerberusApplication::getVisit();
+		
+		$visit->set(ChConfigurationPage::ID, 'slack');
+		
+		$params = array(
+			'consumer_key' => DevblocksPlatform::getPluginSetting('wgm.slack','consumer_key',''),
+			'consumer_secret' => DevblocksPlatform::getPluginSetting('wgm.slack','consumer_secret',''),
+		);
+		$tpl->assign('params', $params);
+
+		$tpl->display('devblocks:wgm.slack::setup/index.tpl');
+	}
+	
+	function saveJsonAction() {
+		try {
+			@$consumer_key = DevblocksPlatform::importGPC($_REQUEST['consumer_key'],'string','');
+			@$consumer_secret = DevblocksPlatform::importGPC($_REQUEST['consumer_secret'],'string','');
+			
+			if(empty($consumer_key) || empty($consumer_secret))
+				throw new Exception("Both the 'Client ID' and 'Client Secret' are required.");
+			
+			DevblocksPlatform::setPluginSetting('wgm.slack', 'consumer_key', $consumer_key);
+			DevblocksPlatform::setPluginSetting('wgm.slack', 'consumer_secret', $consumer_secret);
+			
+			echo json_encode(array('status'=>true, 'message'=>'Saved!'));
+			return;
+			
+		} catch (Exception $e) {
+			echo json_encode(array('status'=>false, 'error'=>$e->getMessage()));
+			return;
+		}
+	}
+};
+endif;
+
+class ServiceProvider_Slack extends Extension_ServiceProvider implements IServiceProvider_OAuth, IServiceProvider_HttpRequestSigner {
+	const ID = 'wgm.slack.service.provider';
+
+	private function _getAppKeys() {
+		$consumer_key = DevblocksPlatform::getPluginSetting('wgm.slack','consumer_key','');
+		$consumer_secret = DevblocksPlatform::getPluginSetting('wgm.slack','consumer_secret','');
+		
+		if(empty($consumer_key) || empty($consumer_secret))
+			return false;
+		
+		return array(
+			'key' => $consumer_key,
+			'secret' => $consumer_secret,
+		);
+	}
+	
+	function renderPopup() {
+		@$view_id = DevblocksPlatform::importGPC($_REQUEST['view_id'], 'string', '');
+		
+		$url_writer = DevblocksPlatform::getUrlService();
+		
+		// [TODO] Report about missing app keys
+		if(false == ($app_keys = $this->_getAppKeys()))
+			return false;
+		
+		$oauth = DevblocksPlatform::getOAuthService($app_keys['key'], $app_keys['secret']);
+		
+		// Persist the view_id in the session
+		$_SESSION['oauth_view_id'] = $view_id;
+		$_SESSION['oauth_state'] = CerberusApplication::generatePassword(24);
+		
+		// OAuth callback
+		$redirect_url = $url_writer->write(sprintf('c=oauth&a=callback&ext=%s', ServiceProvider_Slack::ID), true);
+
+		$url = sprintf("%s?response_type=code&client_id=%s&redirect_uri=%s&scope=%s&state=%s",
+			'https://slack.com/oauth/authorize',
+			rawurlencode($app_keys['key']),
+			rawurlencode($redirect_url),
+			rawurlencode('channels:read chat:write:bot chat:write:user im:read im:write users:read'), // identity.basic 
+			rawurlencode($_SESSION['oauth_state'])
+		);
+		
+		header('Location: ' . $url);
+	}
+	
+	function oauthCallback() {
+		@$view_id = $_SESSION['oauth_view_id'];
+		@$oauth_state = $_SESSION['oauth_state'];
+		
+		@$code = DevblocksPlatform::importGPC($_REQUEST['code'], 'string', '');
+		@$state = DevblocksPlatform::importGPC($_REQUEST['state'], 'string', '');
+		@$error = DevblocksPlatform::importGPC($_REQUEST['error'], 'string', '');
+		
+		$active_worker = CerberusApplication::getActiveWorker();
+		$url_writer = DevblocksPlatform::getUrlService();
+		
+		$redirect_url = $url_writer->write(sprintf('c=oauth&a=callback&ext=%s', ServiceProvider_Slack::ID), true);
+		
+		if(false == ($app_keys = $this->_getAppKeys()))
+			return false;
+		
+		if(!empty($error))
+			return false;
+		
+		$access_token_url = 'https://slack.com/api/oauth.access';
+		
+		$oauth = DevblocksPlatform::getOAuthService($app_keys['key'], $app_keys['secret']);
+		$oauth->setTokens($code);
+		
+		$params = $oauth->getAccessToken($access_token_url, array(
+			'code' => $code,
+			'redirect_uri' => $redirect_url,
+			'client_id' => $app_keys['key'],
+			'client_secret' => $app_keys['secret'],
+		));
+		
+		if(!is_array($params) || !isset($params['access_token']) || !isset($params['user_id'])) {
+			return false;
+		}
+		
+		$oauth->setTokens($params['access_token']);
+		
+		$label = 'Slack';
+		
+		// Load their profile
+		
+		$url = sprintf('https://slack.com/api/users.info?user=%s&token=%s',
+			rawurlencode($params['user_id']),
+			rawurlencode($params['access_token'])
+		);
+		$ch = DevblocksPlatform::curlInit($url);
+		
+		if(false == ($out = DevblocksPlatform::curlExec($ch)))
+			return false;
+		
+		curl_close($ch);
+		
+		if(false == ($json = json_decode($out, true)))
+			return false;
+		
+		// Die with error
+		if(!is_array($json) || !isset($json['user']) && !is_array($json['user']) && !isset($json['user']['name']))
+			return false;
+		
+		$label .= sprintf(": %s", $json['user']['name']);
+		
+		// Save the account
+		
+		$id = DAO_ConnectedAccount::create(array(
+			DAO_ConnectedAccount::NAME => $label,
+			DAO_ConnectedAccount::EXTENSION_ID => ServiceProvider_Slack::ID,
+			DAO_ConnectedAccount::OWNER_CONTEXT => CerberusContexts::CONTEXT_WORKER,
+			DAO_ConnectedAccount::OWNER_CONTEXT_ID => $active_worker->id,
+		));
+		
+		DAO_ConnectedAccount::setAndEncryptParams($id, $params);
+		
+		if($view_id) {
+			echo sprintf("<script>window.opener.genericAjaxGet('view%s', 'c=internal&a=viewRefresh&id=%s');</script>",
+				rawurlencode($view_id),
+				rawurlencode($view_id)
+			);
+			
+			C4_AbstractView::setMarqueeContextCreated($view_id, CerberusContexts::CONTEXT_CONNECTED_ACCOUNT, $id);
+		}
+		
+		echo "<script>window.close();</script>";
+	}
+	
+	function authenticateHttpRequest(Model_ConnectedAccount $account, &$ch, &$verb, &$url, &$body, &$headers) {
+		$credentials = $account->decryptParams();
+		
+		if(
+			!isset($credentials['access_token'])
+		)
+			return false;
+		
+		if(false == ($url_parts = parse_url($url)))
+			return false;
+		
+		if(false !== stripos($url,'?')) {
+			$url .= '&token=' . rawurlencode($credentials['access_token']);
+		} else {
+			$url .= '?token=' . rawurlencode($credentials['access_token']);
+		}
+		
+		curl_setopt($ch, CURLOPT_URL, $url);
+		
+		return true;
+	}
+}
